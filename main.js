5a341c146df65b0ca8b8018c3bcd78e9c72907e7
Ziad <ziadabdelaziz99@gmail.com>
2024-12-07 16:34:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+document.writeln("<h1>Hello In Js</h1>")
+document.writeln("My Name Is Ziad")
+// window.alert('Welcome To Our Site')
+console.log('My Name Is Ziad Mohamed')
+console.log('My BirthDate Is 10/1/2003')
+
+
+var num1=+prompt('Enter First Value Which Is Equal 15')
+var num2=+prompt('Enter Second Value Which Is Equal 3')
+sum =num1+num2
+document.writeln('<h1>The Sum Result Is</h1>')
+document.writeln(sum)
+
+var num1=+prompt('Enter First Value Which Is Equal 15')
+var num2=+prompt('Enter Second Value Which Is Equal 3')
+sum =num1-num2
+document.writeln('<h1>The Subtraction Result Is</h1>')
+document.writeln( sum)
+
+var num1=+prompt('Enter First Value Which Is Equal 15')
+var num2=+prompt('Enter Second Value Which Is Equal 3')
+sum =num1*num2
+document.writeln('<h1>The Multiplication Result Is</h1>')
+document.writeln(sum)
+
+var num1=+prompt('Enter First Value Which Is Equal 15')
+var num2=+prompt('Enter Second Value Which Is Equal 3')
+sum =num1/num2
+document.writeln('<h1>The Division Result Is</h1>')
+document.writeln(sum)
+
+var num1=+prompt('Enter First Value Which Is Equal 15')
+var num2=+prompt('Enter Second Value Which Is Equal 3')
+sum =num1**num2
+document.writeln('<h1>The Power Result Is</h1>')
+document.writeln(sum)
+
+var num1=+prompt('Enter First Value Which Is Equal 15')
+var num2=+prompt('Enter Second Value Which Is Equal 3')
+sum =num1%num2
+document.writeln('<h1>The Remainder Of The Division Result Is</h1>')
+document.writeln(sum)
+
+
+var equal=num1==num2
+document.writeln('<h1>Boolean Result</h1>')
+document.writeln(equal)
+
+var equal=num1>num2
+document.writeln('<h1>Boolean Result</h1>')
+document.writeln(equal)
